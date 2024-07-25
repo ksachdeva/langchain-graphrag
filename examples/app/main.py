ecfb4f7d2594af6411e171f9ebc9470baaee615b
Kapil Sachdeva <ksachdeva17@gmail.com>
2024-07-25 19:36:12 +0000

    fix(examples/app) - suppress the log level for httpx

--- examples/app/main.py
+++ examples/app/main.py
@@ -1,3 +1,4 @@
+import logging
 from dotenv import load_dotenv
 
 from omegaconf import DictConfig, OmegaConf
@@ -6,6 +7,12 @@ import hydra
 
 @hydra.main(version_base="1.3", config_path="./configs", config_name="indexing.yaml")
 def indexer(cfg):
+
+    # some how seeing httpx INFO LEVEL for requests
+    # disabling it here for now.
+    # TODO: should be able to do it via hydra config
+    logger = logging.getLogger("httpx")
+    logger.setLevel(logging.WARNING)
 
     load_dotenv()
 
