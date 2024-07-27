307b355a05080251d372557152ae75c3b351eae5
Kapil Sachdeva <ksachdeva17@gmail.com>
2024-07-27 19:41:31 +0000

    feat - in HierarchicalLeidenCommunityDetector, return the list of clustered graphs

--- langchain_graphrag/indexing/graph_clustering/community_detector.py
+++ langchain_graphrag/indexing/graph_clustering/community_detector.py
@@ -1,9 +1,14 @@
-from typing import TypedDict, TypeAlias, NewType
+from typing import NewType
+from typing import Sequence
+
+from random import Random
 
 import networkx as nx
 
 from graspologic.partition import hierarchical_leiden
 from graspologic.partition import HierarchicalCluster, HierarchicalClusters
+
+from langchain_graphrag.utils.uuid import gen_uuid
 
 from .utils import stable_largest_connected_component
 
@@ -11,6 +16,55 @@ CommunityLevel = NewType("CommunityLevel", int)
 CommunityId = NewType("CommunityId", str)
 NodeName = NewType("NodeName", str)
 CommunityDetectionResult = dict[CommunityLevel, dict[CommunityId, list[NodeName]]]
+Communities = list[tuple[CommunityLevel, CommunityId, list[NodeName]]]
+
+
+def apply_level(
+    source_graph: nx.Graph,
+    communities: Communities,
+    level: CommunityLevel,
+    seed=0xF001,
+) -> nx.Graph:
+    random = Random(seed)  # noqa S311
+
+    graph = source_graph.copy()
+
+    for community_level, community_id, nodes in communities:
+        if level != community_level:
+            continue
+
+        for node in nodes:
+            graph.nodes[node]["cluster"] = community_id
+            graph.nodes[node]["level"] = level
+
+    # add node degree
+    for node_degree in graph.degree:
+        graph.nodes[str(node_degree[0])]["degree"] = int(node_degree[1])
+
+    # add node uuid and incremental record id (a human readable id used as reference in the final report)
+    for index, node in enumerate(graph.nodes()):
+        graph.nodes[node]["human_readable_id"] = index
+        graph.nodes[node]["id"] = str(gen_uuid(random))
+
+    # add ids to edges
+    for index, edge in enumerate(graph.edges()):
+        graph.edges[edge]["id"] = str(gen_uuid(random))
+        graph.edges[edge]["human_readable_id"] = index
+        graph.edges[edge]["level"] = level
+
+    return graph
+
+
+def apply_clustering(
+    source_graph: nx.Graph,
+    levels: Sequence[CommunityLevel],
+    communities: Communities,
+) -> list[tuple[CommunityLevel, nx.Graph]]:
+    graph_level_pairs: list[tuple[CommunityLevel, nx.Graph]] = []
+    for level in levels:
+        graph = apply_level(source_graph, communities, level)
+        graph_level_pairs.append((level, graph))
+    return graph_level_pairs
 
 
 class HierarchicalLeidenCommunityDetector:
@@ -24,7 +78,7 @@ class HierarchicalLeidenCommunityDetector:
         self._max_cluster_size = max_cluster_size
         self._seed = seed
 
-    def run(self, graph: nx.Graph) -> CommunityDetectionResult:
+    def run(self, graph: nx.Graph) -> list[tuple[CommunityLevel, nx.Graph]]:
         if self._use_lcc:
             graph = stable_largest_connected_component(graph)
 
@@ -53,4 +107,12 @@ class HierarchicalLeidenCommunityDetector:
                     result[community_id] = []
                 result[community_id].append(node_id)
 
-        return results_by_level
+        communities: Communities = []
+        for level in results_by_level:
+            for cluster_id, nodes in results_by_level[level].items():
+                communities.append((level, cluster_id, nodes))
+
+        # time to apply clustering
+        graph_level_pairs = apply_clustering(graph, levels, communities)
+
+        return graph_level_pairs
