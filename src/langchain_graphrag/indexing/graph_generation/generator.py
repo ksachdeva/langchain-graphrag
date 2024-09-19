5af7b1c16c342c78af4a422eaa19ad0f4ad0b30c
Kapil Sachdeva <ksachdeva17@gmail.com>
2024-09-19 21:48:07 +0000

    feat - in GraphGenerator, add an optional callable to give an opportunity to operate on the graph before it is passed to the DescriptionSummarizer

--- src/langchain_graphrag/indexing/graph_generation/generator.py
+++ src/langchain_graphrag/indexing/graph_generation/generator.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing import Callable
 
 import networkx as nx
 import pandas as pd
@@ -14,15 +15,22 @@ class GraphGenerator:
         er_extractor: EntityRelationshipExtractor,
         graphs_merger: GraphsMerger,
         er_description_summarizer: EntityRelationshipDescriptionSummarizer,
+        graph_sanitizer: Callable[[nx.Graph], nx.Graph] | None = None,
     ):
         self._er_extractor = er_extractor
         self._graphs_merger = graphs_merger
+        self._graph_sanitizer = graph_sanitizer
         self._er_description_summarizer = er_description_summarizer
 
     def run(self, text_units: pd.DataFrame) -> tuple[nx.Graph, nx.Graph]:
         er_graphs = self._er_extractor.invoke(text_units)
         er_merged_graph = self._graphs_merger(er_graphs)
-        er_summarized_graph = self._er_description_summarizer.invoke(
-            deepcopy(er_merged_graph)
+        er_sanitized_graph = (
+            self._graph_sanitizer(er_merged_graph)
+            if self._graph_sanitizer
+            else er_merged_graph
         )
-        return er_merged_graph, er_summarized_graph
+        er_summarized_graph = self._er_description_summarizer.invoke(
+            deepcopy(er_sanitized_graph)
+        )
+        return er_sanitized_graph, er_summarized_graph
